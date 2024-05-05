1637df3c77a2bb8fc20fd94f04de0e11bae00edc
Tobi Adeleke <tobiaadeleke@gmail.com>
2024-05-06 00:00:32 +0100

    add api and controller for users login and register

--- src/application/users/controller.py
+++ src/application/users/controller.py
@@ -1,18 +1,93 @@
 from datetime import datetime, timedelta
-from flask import request, make_response
+import bcrypt
+from flask import (
+    request, make_response,redirect, url_for, session
+)
+
+from application import JWT_SECRETKEY
+from application import db
 from .models import User
 
 class LoginController:
     """ 
+    TODO
      Handles Login, creating access-token, setting cookies, and log-out
     """
-    pass
+    def login(self):   
+        # status, userResponse = UserController().find()
+        # if status:
+        #     session.clear()
+        #     session['user_id'] = userResponse.id 
+        #     return userResponse 
+        # return 
+        pass
 
 class UserController:
-    pass 
+    def addNew(self):
+       
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        error = None
 
-class DataHandler:
-    pass 
+    
+        if not username:
+            error = 'Username is required.'
+        elif not password: 
+            error = 'Password is required.'  
+        elif not email: 
+            error = 'Email is required.'
+     
+        if error is None:
+            try:
+                user = User(
+                    username=username,
+                    email=email,
+                    password=bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt())
+                )
+                db.session.add(user)
+                db.session.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered"
+            else:       
+                return True
+        return error
+
+    def find(self):
+        email = request.form.get('email')
+        password = request.form.get('password')
+        error = None
+     
+        try: 
+            user = db.session.query(User).filter(User.email == email).first()
+       
+        except: 
+            error = 'User Not Found'
+
+        if user is None:
+            error = 'Incorrect Username'
+        elif not bcrypt.checkpw(password.encode('utf-8'), user.password):
+            error = 'Incorrect Password'
+        
+        if error is None:
+        
+            return (True, user)
+        
+        return (False, error)
+    
+    def findById(self, user_id=None):
+        user, error = None, None
+ 
+        if user_id :
+            try: 
+                user = db.session.query(User).filter(User.id == user_id).first()
+            except: 
+                error = 'User Not Found'  
+ 
+        if error is None:
+            return (True, user) 
+        return (False, error)
+
 
 class LoginErr:
     pass
--- src/application/users/user_api.py
+++ src/application/users/user_api.py
@@ -1,26 +1,46 @@
 from flask import (
-	Blueprint, flash, g, redirect, render_template, request, url_for, send_from_directory, current_app
+	Blueprint, flash, g, redirect, render_template, request, url_for,session
 )
-
+from .controller import UserController
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/login')
+
+@bp.route('/login', methods=['GET','POST'])
 def login():
-   
+    if request.method == 'POST': 
+        status, userresponse = UserController().find()
+        if status:
+            session.clear()
+            session['user_id'] = userresponse.id 
+            return redirect(url_for('task.index')) 
+            
+    
+        flash(userresponse)
     return render_template('auth/login.html')
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     
     if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('floatingPassword')
-        error = None
-        
-        # TODO complete to add to database
-        if not username:
-            error = 'Username is required.'
-        elif not password: 
-            error = 'Password is required.'
 
-    return render_template('auth/sign-up.html')
+        userresponse = UserController().addNew()
+        if userresponse:
+            return redirect(url_for('auth.login')) 
+        else:
+            flash(userresponse)
+    return render_template('auth/register.html')
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('auth.login'))
+   
+@bp.before_app_request
+def load_current_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        status, userresponse = UserController().findById(user_id)
+        if status:
+            g.user = userresponse
