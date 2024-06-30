e6a8bb18526d827ac65ba4f71df671531396d9a7
Tobi Adeleke <tobiaadeleke@gmail.com>
2024-06-30 12:45:40 +0100

    amend stylesheet and related html files

--- src/manage.py
+++ src/manage.py
@@ -1,5 +1,5 @@
 from flask.cli import FlaskGroup
-from application import app,db
+from application import app, db
 from application.users.models import User
 from application.buddy_task.models import Task, BuddyContact
 from datetime import datetime, timedelta
@@ -15,18 +15,18 @@ def create_db():
     db.create_all()
     db.session.commit()
 
+
 @cli.command("seed_db")
 def seed_db():
 
     db.session.add(User("rewTobi", "rewTobi@gmail.com", "preq9004"))
     db.session.add(User("Jenny", "Jenny@outlook.com", "frrry90056"))
     # db.session.add(Task(datetime.today() ,"Go Dancing", 1,"Open", datetime.today()+timedelta(days=5) ))
-    db.session.add(Task( "Exercise","Go Dancing", 1, datetime.today()+timedelta(days=5) ))
-    db.session.add(Task("Exercise", "Go Skiing", 1, datetime.today()+timedelta(days=10) ))
-    db.session.add(Task("Academy", "Reading", 2, datetime.today()+timedelta(days=20) ))
+    db.session.add(Task("Exercise", "Go Dancing", 1, datetime.today()+timedelta(days=5)))
+    db.session.add(Task("Exercise", "Go Skiing", 1, datetime.today()+timedelta(days=10)))
+    db.session.add(Task("Academy", "Reading", 2, datetime.today()+timedelta(days=20)))
 
     db.session.commit()
-
 
 
 if __name__ == "__main__":
